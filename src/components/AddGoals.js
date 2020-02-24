7fd7d15d0f4018b3e22986a74e7f46b942c6d58d
spark1006 <soominpark1006@gmail.com>
2020-02-24 00:31:26 -0500

    tabs and add goals button

---
+++ src/components/AddGoals.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AddGoals extends Component {
+
+  render() {
+    return (
+      <div className="addGoals">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default AddGoals;
--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import AddGoals from "./AddGoals";
+import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 class Goals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      addGoal: false
+    };
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  }
+
+  handleAdd = () => {
+    this.setState({ addGoal: true });
+  }
+
   render() {
     return (
-      <div>
-        <p>Goals page</p>
+      <div className="goalsPage">
+        <Grid container>
+          <Grid item xs={3}>
+            <Sidebar />
+          </Grid>
+          <Grid item xs={8}>
+            <div className="header paddingTop30px">Setting Goals</div>
+            <Paper square>
+              <Tabs value={this.state.value} indicatorColor="primary" textColor="primary" onChange={this.handleChange}>
+                <Tab label="Current Goals" />
+                <Tab label="Completed Goals" />
+              </Tabs>
+              <Button variant="contained" color="primary" style={{float: "right"}} onClick={this.handleAdd}>
+                  + Add Goal
+              </Button>
+            </Paper>
+            <div>
+              { this.state.addGoal && <AddGoals /> }
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
   }
