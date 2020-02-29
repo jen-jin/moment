65f243258b144db2adaddf7e7ef915f2f64da10d
spark1006 <soominpark1006@gmail.com>
2020-02-29 13:40:48 -0500

    add logo and refactor sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { List, ListItem, ListItemText, Drawer, withStyles } from '@material-ui/core';
+import { Drawer, withStyles } from '@material-ui/core';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 import { Link } from "react-router-dom";
+import logo from "../img/logo.svg";
 
 const drawerWidth = 240;
 
@@ -15,21 +18,36 @@ const styles = theme => ({
   drawerPaper: {
     width: drawerWidth
   },
+  logo: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 8,
+    paddingBottom: 8
+  }
 });
 
 class Sidebar extends Component {
+  state = {
+    value: 0
+  };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
         <Drawer className={classes.drawer} variant="permanent" anchor="left" classes={{paper: classes.drawerPaper,}}>
-          <List>
-            {['Dashboard', 'Goals', 'Reflection', 'Resources'].map((text) => (
-              <ListItem key={text} component={Link} to={"/" + text.toLowerCase()}>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
+            <div className={classes.logo}>
+              <img src={logo} alt="Logo" />
+            </div>
+            <Tabs orientation="vertical" value={this.state.value} onChange={this.handleChange} indicatorColor={"primary"}>
+              {["Dashboard", "Goals", "Reflection", "Resources"].map((text) => (
+                <Tab label={text} component={Link} to={"/" + text.toLowerCase()} />
+              ))}
+            </Tabs>
         </Drawer>
       </div>
     );
