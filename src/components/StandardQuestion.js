ff02c768450aad29dd2e257e336a9c53669f053f
Jenny Jin <jenjenjin@gmail.com>
2020-03-02 12:01:09 -0500

    UI changes for buttons and standard question

--- src/components/StandardQuestion.js
+++ src/components/StandardQuestion.js
@@ -4,17 +4,17 @@ import TextField from '@material-ui/core/TextField';
 class StandardQuestion extends Component {
     render(props) {
         return (
-            <div className="paddingTop30px">
-                <div className="body">{this.props.question}</div>
-                <div className="paddingTop10px" />
+            <div className="paddingTop30px" style={{ width: 900 }}>
+                <div className="body bodyBold">{this.props.question}</div>
+                <div className="paddingTop10px"/>
                 <TextField 
                 id="outlined-multiline-static"
                 multiline
                 rows="5"
                 variant="outlined"
-                placeholder="Type your answer here"
+                placeholder={this.props.placeholder}
                 inputProps={{
-                  style: {fontFamily: 'Open Sans', fontSize: 15, width: 500} 
+                  style: {fontFamily: 'Open Sans', fontSize: 15, width: 800} 
                 }}
               />
             </div>
