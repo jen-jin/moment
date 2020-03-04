737faf53a83b5c82e4cf5f9276d7cb35abcdbca0
Jenny Jin <jenjenjin@gmail.com>
2020-03-04 16:41:06 -0500

    WIP reflection table

--- src/components/Reflection.js
+++ src/components/Reflection.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
-import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
 import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import { REFLECTION_PATH, DEFAULT_HEADERS, SUCCESS } from "../constants";
 
 class Reflection extends Component {
   static contextType = AuthContext;
@@ -10,11 +19,64 @@ class Reflection extends Component {
     super(props);
 
     this.state = {
-      step: 1,
-      goals: []
+      reflections: [],
+      page: 0,
+      rowsPerPage: 10
     };
 
     this.createReflection = this.createReflection.bind(this);
+    this.handleChangePage = this.handleChangePage.bind(this);
+    this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
+    this.createTableData = this.createTableData.bind(this);
+  }
+
+  // MARK: - Lifecycle
+
+  componentDidMount() {
+    const { userId } = this.context;
+
+    console.log("User ID: " + userId);
+
+    axios
+      .get(REFLECTION_PATH + "/" + parseInt("14"), {
+        // Temporarily changing it to 14 until we have the APIs sorted out
+        headers: DEFAULT_HEADERS
+      })
+      .then(
+        response => {
+          if (response.data.status == SUCCESS && response.data.data != []) {
+            this.setState({
+              reflections: response.data.data
+            });
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  handleChangePage(event, newPage) {
+    this.setState({
+      page: newPage
+    });
+  }
+
+  handleChangeRowsPerPage(event) {
+    this.setState({
+      rowsPerPage: +event.target.value,
+      page: 0
+    })
+  }
+  
+  createTableData(reflection) {
+    const title = reflection.reflection;
+    const dateCreated = reflection.timestamp;
+    const dateModified = reflection.timestamp;
+
+    console.log("Row Title: " + title + "Row Date: " + dateCreated);
+
+    return { title, dateCreated, dateModified };
   }
 
   createReflection() {
@@ -22,6 +84,40 @@ class Reflection extends Component {
   }
 
   render() {
+    const { reflections, page, rowsPerPage } = this.state;
+    
+    const columns = [
+      { id: "title", label: "Name", minWidth: 170 },
+      {
+        id: "dateCreated",
+        label: "Date Created",
+        minWidth: 170,
+        align: "right"
+      },
+      {
+        id: "lastModified",
+        label: "Last Modified",
+        minWidth: 170,
+        align: "right"
+      },
+      {
+        id: "actions",
+        label: "Actions",
+        minWidth: 170,
+        align: "right"
+      }
+    ];
+
+    // TODO: Issues is in initializing the rows
+    const rows = [];
+
+    reflections.map((reflection, index) => {
+      console.log("Reflection " + index);
+      rows.concat(this.createTableData(reflection));
+    });
+
+    console.log("Rows: " + rows.length)
+    
     return (
       <div className="reflectionLog">
         <Grid
@@ -39,12 +135,83 @@ class Reflection extends Component {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </div>
-            <div className="button buttonWidth150px borderRadius25px marginTop30px marginBottom30px rightAlign" onClick={this.createReflection}>
+            <div
+              className="button buttonWidth150px borderRadius25px marginTop30px marginBottom30px rightAlign"
+              onClick={this.createReflection}
+            >
               + Add Reflection
             </div>
           </Grid>
           <Grid item xs={8}>
-          <div className="helper paddingTop30px">You haven't created any reflections yet. Click the + Add Reflection button to create a new reflection</div>
+            {reflections.length > 0 && (
+              <Paper className="table">
+                <TableContainer
+                  className="table-container"
+                  style={{ maxHeight: 450 }}
+                >
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map(column => (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                          >
+                            {column.label}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                        )
+                        .map(row => {
+                          return (
+                            <TableRow
+                              hover
+                              role="checkbox"
+                              tabIndex={-1}
+                              key={row.code}
+                            >
+                              {columns.map(column => {
+                                const value = row[column.id];
+                                return (
+                                  <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                  >
+                                    {value}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                />
+              </Paper>
+            )}
+
+            {reflections.length == 0 && (
+              <div className="helper paddingTop30px">
+                You haven't created any reflections yet. Click the + Add
+                Reflection button to create a new reflection
+              </div>
+            )}
           </Grid>
         </Grid>
       </div>
